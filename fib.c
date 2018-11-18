ca2789e44ec74ba963ef8209eb4e6d5cb6b481af
Awais Jafar <awaisjafar@gmail.com>
2018-11-18 13:00:00 -0600

    Fibonacci source code

---
+++ fib.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "profile.c"
+
+int fib(int x) {
+  if (x == 0)
+   return 0;
+  else if (x == 1) 
+    return 1;
+  return fib(x - 1) + fib(x - 2);
+}
+
+int main(int argc, char *argv[]) {
+ 
+  /* Timing the Fibonacci calculation for numbers from 1 to 45 */
+  time_start("Fibcalc");
+  unsigned int i;
+  for (i = 1; i <= 45; ++i) {
+    printf("Fibonacci for %d : %d\n", i, fib(i));
+  }
+  time_end("Fibcalc");
+  time_summary();
+  
+  return 0;
+}
